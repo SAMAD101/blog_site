3b7c3d6a8b2aa0d5d67c9b9480ce03d30dba7e15
Sam <asamadans@gmail.com>
2024-01-21 00:57:57 +0530

    fixed crispy forms

--- django_project/settings.py
+++ django_project/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
     'crispy_forms',
+    'crispy_bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
