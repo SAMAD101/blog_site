6259800e4185f2fcd9f0c93e280d24b69ed47498
Sam <asamadans@gmail.com>
2024-01-21 19:55:36 +0530

    postgres setup changes (dev)

--- django_project/settings.py
+++ django_project/settings.py
@@ -79,9 +79,14 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'users_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
+    
 }
 
 
